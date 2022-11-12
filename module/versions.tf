c340055e9c30fb4ba695c2e0d093fcd1c7516271
balloray <63379157+balloray@users.noreply.github.com>
2022-11-11 22:22:51 -0600

    Update versions.tf

--- module/versions.tf
+++ module/versions.tf
@@ -8,16 +8,3 @@ terraform {
     }
   }
 }
-
-# required helm provider for this module 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-  #version = "1.3.2" #previous version was "0.10.6"
-}
-
-provider "kubernetes" {
-  #version = "1.3.2" #previous version was "0.10.6"
-  config_path = "~/.kube/config"
-}
