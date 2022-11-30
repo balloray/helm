3e4979455a06c60732c21e8ed066aa724a0104ac
rasheed balogun <balloray@gmail.com>
2022-11-30 02:41:53 +0000

    x

--- local/module/variable.tf
+++ local/module/variable.tf
@@ -8,16 +8,6 @@ variable "chart_path" {
   description = "-(Required) Chart location or chart name <stable/example>"
 }
 
-variable "chart_version" {
-  description = "-(Optional) Specify the exact chart version to install"
-  default     = "17.0.37"
-}
-
-variable "chart_repo" {
-  default     = "https://charts.bitnami.com/bitnami"
-  description = "-(Optional) Provide the remote helm charts repository."
-}
-
 variable "timeout" {
   default = "400"
 }
