4b66be6b3f5bb3a3a0fb6c4e6d22e4bbed168ea9
balloray <63379157+balloray@users.noreply.github.com>
2023-03-31 11:08:22 -0500

    Update module.tf

--- local/module.tf
+++ local/module.tf
@@ -2,6 +2,7 @@ module "chart" {
   source                  = "github.com/balloray/helm-chart/module"
   chart_name              = "external-dns"
   chart_path              = "external-dns"
+  chart_ns                = "ns"
   chart_override_values   = <<EOF
 provider: google
 EOF
